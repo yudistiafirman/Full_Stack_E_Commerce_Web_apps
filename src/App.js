b0cf00869924e095195010ca0b266e16d3296457
ansorudin <ahmadansorudin@gmail.com>
2020-11-18 21:57:28 +0700

    add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import {applyMiddleware, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
+import rootReducer from './Redux/Reducer'
 
 
-function App() {
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div>
+        INI APP
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
+
