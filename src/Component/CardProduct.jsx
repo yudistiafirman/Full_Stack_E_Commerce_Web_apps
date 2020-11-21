9cf32596baeea4763b4882dc25e24f3d792978c2
ansorudin <ahmadansorudin@gmail.com>
2020-11-21 18:31:07 +0700

    add breadscrumb in list product

--- src/Component/CardProduct.jsx
+++ src/Component/CardProduct.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faSquare, faStar } from '@fortawesome/free-solid-svg-icons'
 
 const CardProduct = () => {
     const [onHover, setOnhover] = useState(false)
+
+    const [imageSlide, setImageSlide] = useState(true)
+
     return (
         <div className='col-md-3 ' style={{height : 500, padding : 10, backgroundSize: 'cover',backgroundPosition: 'center'}}>
-            <div className='card-container' onMouseEnter={() => setOnhover(true)} onMouseLeave={() => setOnhover(false)} >
-                <div className=' container-image w-100' style={{padding : 10}}>
+            <div className='card-container' onMouseEnter={() => {setOnhover(true)}} onMouseLeave={() => {setOnhover(false)}} >
+                <div className=' container-image w-100' style={{padding : 10}} onMouseEnter={() => setImageSlide(false)} onMouseLeave={() => setImageSlide(true)}>
                     <img 
                     className=''
                     style={{maxWidth : '100%', height : 'auto', position: 'relative' }}
-                    src='https://images.freshop.com/00041383090363/7f213cf018f31573c7f942fd564d255a_large.png'
-                    
+                    src={imageSlide ? 'https://dynamic.zacdn.com/9x-iHypJa1fN_TGRcsz562oR6SU=/fit-in/762x1100/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/quiksilver-3727-8123452-1.jpg' : 'https://dynamic.zacdn.com/sVNGtZBV4nqK6j1nAV87tIY7YlI=/fit-in/762x1100/filters:quality(90):fill(ffffff)/http://static.id.zalora.net/p/quiksilver-3730-8123452-3.jpg'}
                     />
                 </div>
                 <span style={{position : 'absolute',top : 10, left : 10,backgroundColor : 'salmon', padding : 5, fontSize : 12, color : 'white', borderTopLeftRadius : 10}}>
@@ -41,6 +43,12 @@ const CardProduct = () => {
                         <div className='a-button-inner'>
                             <p className='a-button-text'>Add to Cart</p>
                         </div>
+                    </div>
+                </div>
+                <div className='choice-varian-container'>
+                    <div>
+                        <FontAwesomeIcon icon={faSquare} className='icon-varian' style={{color : 'red'}} />
+                        <FontAwesomeIcon icon={faSquare} className='icon-varian' style={{color : 'black'}} />
                     </div>
                 </div>
             </div>
--- src/Pages/ListProduct/ListProduct.jsx
+++ src/Pages/ListProduct/ListProduct.jsx
@@ -1,89 +1,164 @@
-import { faCheck, faChevronCircleDown, faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faSlidersH, faStar} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 import CardProduct from '../../Component/CardProduct'
 import './CardProduct.css'
 import './SortProduct.css'
-import {  Button, CardBody, Card, Collapse } from 'reactstrap';
-
+import { Collapse } from 'reactstrap';
 
 
 const ListProduct = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState({
+        child_1 : false,
+        child_2 : false
+    });
+    const [cek, setCek] = useState([
+        {
+            name : 'Sayuran',
+            id : 1
+        },
+        {
+            name : 'Buah',
+            id : 2
+        },
+        {
+            name : 'Kacang-kacangan',
+            id : 3
+        }
+    ])
 
-    const [cek, setCek] = useState(0)
+    const [rating, setRating] = useState([
+        {
+            id : 1,
+            star : 1
+        },
+        {
+            id : 2,
+            star : 2
+        },
+        {
+            id : 3,
+            star : 3
+        },
+        {
+            id : 4,
+            star : 4
+        },
+        {
+            id : 5,
+            star : 5
+        }
+    ])
 
-    const toggle = () => setIsOpen(!isOpen);
+    const onHandleCheckCategory = (e) => {
+        
+        let handle = cek
+        handle.forEach(val => {
+        if (val.name === e.target.value)
+            val.isChecked =  e.target.checked
+        })
+        setCek(handle)
+        console.log(cek)
+    }
+    const onHandleCheckRating = (e) => {
+        
+        let handle = rating
+        handle.forEach(val => {
+        if (val.name === e.target.value)
+            val.isChecked =  e.target.checked
+        })
+        setRating(handle)
+        console.log(rating)
+    }
 
-    console.log(cek)
     
     return (
         <div className='container mt-5'>
-            {/* <div style={{display : 'flex', alignItems : 'center'}}>
-                <span className='border container-check-box' >
-                    <FontAwesomeIcon icon={faCheck} style={{fontSize : 12, margin : 0, padding : 0}}/>
+            <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginBottom : 60}}>
+                <img 
+                style={{width : '100%', height : 250, objectFit : 'cover'}}
+                src='https://images.unsplash.com/photo-1513617332477-a365e97b52a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80' />
+            </div>
+            <div>
+                <span>
+                    Home
                 </span>
-                <span style={{marginLeft : 5 ,display : 'flex', justifyContent : 'center', alignItems : 'center'}} >
-                    <FontAwesomeIcon icon={faStar} style={{fontSize : 14, color : 'orange'}}/>
-                    <p style={{fontSize : 14, marginLeft : 5}}>5 ke atas</p>
+                <span>
+                    Home
                 </span>
-            </div> */}
+            </div>
             <div className='row'>
-                <div className='col-md-3'>
-                    <div>
-                        <div onClick={toggle} style={{display : 'flex',  alignItems : 'center', justifyContent : 'space-between', paddingBottom : 10}} >
-                            <p>Kategori</p>
-                            <FontAwesomeIcon icon={faChevronDown} style={{fontSize : 14}}/>
-                        </div>
-                        <Collapse isOpen={isOpen}>
-                            <span style={{display : 'flex', alignItems : 'center'}}>
-                                <input 
-                                type="checkbox" 
-                                checked={cek} 
-                                onChange={() => setCek(!cek)}  
-                                style={{width : 16, height : 16}}
-                                />
-                                <p style={{marginLeft : 10}}>Sayuran</p>
-                            </span>
-                            <span style={{display : 'flex', alignItems : 'center'}}>
-                                <input 
-                                type="checkbox" 
-                                checked={cek} 
-                                onChange={() => setCek(!cek)}  
-                                style={{width : 16, height : 16}}
-                                />
-                                <p style={{marginLeft : 10}}>Sayuran</p>
-                            </span>
-                            <span style={{display : 'flex', alignItems : 'center'}}>
-                                <input 
-                                type="checkbox" 
-                                checked={cek} 
-                                onChange={() => setCek(!cek)}  
-                                style={{width : 16, height : 16}}
-                                />
-                                <p style={{marginLeft : 10}}>Sayuran</p>
-                            </span>
-                            <span style={{display : 'flex', alignItems : 'center'}}>
-                                <input 
-                                type="checkbox" 
-                                checked={cek} 
-                                onChange={() => setCek(!cek)}  
-                                style={{width : 16, height : 16}}
-                                />
-                                <p style={{marginLeft : 10}}>Sayuran</p>
-                            </span>
-                        </Collapse>
+                <div className='col-md-3' style={{paddingRight : 30, paddingTop : 5}} >
+                    <div style={{display : 'flex', alignItems : 'center', justifyContent : 'space-between', marginBottom : 20}}>
+                        <p style={{fontSize : 20}}>Filter</p>
+                        <FontAwesomeIcon icon={faSlidersH} />
                     </div>
                     <div>
-                        <div onClick={toggle} className='border-top' style={{display : 'flex',  alignItems : 'center', justifyContent : 'space-between', paddingBottom : 10, paddingTop : 10}} >
+                        <div onClick={() => setIsOpen({...isOpen, child_1 : !isOpen.child_1})} style={{display : 'flex',  alignItems : 'center', justifyContent : 'space-between'}} >
                             <p>Kategori</p>
-                            <FontAwesomeIcon icon={faChevronDown} style={{fontSize : 14}}/>
+                            <FontAwesomeIcon icon={isOpen.child_1 ? faChevronUp : faChevronDown} style={{fontSize : 14}}/>
                         </div>
-                        <Collapse>
-                            
+                        <div style={{padding : 10}}>
+                        <Collapse isOpen={isOpen.child_1}>                            
+                            {
+                                cek.map((val,index) => {
+                                    return(
+                                        <span style={{display : 'flex', alignItems : 'center', padding : 3}}>
+                                            <input 
+                                            type="checkbox" 
+                                            checked={val.isChecked}
+                                            onClick={(e) => onHandleCheckCategory(e)}
+                                            style={{width : 16, height : 16}}
+                                            value={val.name}
+                                            />
+                                            <p style={{marginLeft : 10}}>{val.name}</p>
+                                        </span>
+                                    )
+                                })
+                            }
                         </Collapse>
+                        </div>
                     </div>
+                    
+
+                    <div>
+                        <div onClick={() => setIsOpen({...isOpen, child_2 : !isOpen.child_2})} style={{display : 'flex',  alignItems : 'center', justifyContent : 'space-between'}} >
+                            <p>Rating</p>
+                            <FontAwesomeIcon icon={isOpen.child_2 ? faChevronUp : faChevronDown} style={{fontSize : 14}}/>
+                        </div>
+                        <div style={{padding : 10}}>
+                        <Collapse isOpen={isOpen.child_2}>                            
+                            {
+                                rating.map((val,index) => {
+                                    return(
+                                        <span style={{display : 'flex', alignItems : 'center', padding : 3}}>
+                                            <input 
+                                            type="checkbox" 
+                                            checked={val.isChecked}
+                                            onClick={(e) => onHandleCheckRating(e)}
+                                            style={{width : 16, height : 16}}
+                                            value={val.name}
+                                            />
+
+                                            <span style={{marginLeft : 10}}>
+                                            {
+                                            Array.apply(null, {length: val.star}).map(Number.call, Number).map((val) => {
+                                                return(
+                                                    <FontAwesomeIcon icon={faStar} style={{color : 'orange', fontSize : 12}} />
+                                                )
+                                            })
+                                            }
+                                            </span>
+                                            
+                                        </span>
+                                    )
+                                })
+                            }
+                        </Collapse>
+                        </div>
+                    </div>
+                    
                 </div>
                 <CardProduct />
                 <CardProduct />
